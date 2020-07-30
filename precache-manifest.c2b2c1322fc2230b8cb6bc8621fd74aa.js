f869c1106dd3245a86b6fb5565719b55bfc7a3e6
MohammadAli <mh.newday@gmail.com>
2020-07-30 10:49:01 +0430

    Updates

---
+++ precache-manifest.c2b2c1322fc2230b8cb6bc8621fd74aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a535020b4e06b21ba5aa7e08731bc7b2",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "83caab17a28f8ecd2b20",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46cd8c9d74bc6ac85adc",
+    "url": "/todoapp/static/js/2.fbb0a158.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todoapp/static/js/2.fbb0a158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83caab17a28f8ecd2b20",
+    "url": "/todoapp/static/js/main.d80a0681.chunk.js"
+  },
+  {
+    "revision": "0ed5d5c2c5a14bbcac4e",
+    "url": "/todoapp/static/js/runtime-main.e074d08a.js"
+  }
+]);
